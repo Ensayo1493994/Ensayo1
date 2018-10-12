a77266b9f9295b1bb338ed7de1493251c40ef3f8
ebilyperafan <ebilyperafan1911.z@gmail.com>
2018-10-12 16:33:17 -0500

    mostrardatos

--- ProyectoEnsayo/app/src/main/java/ragp/provicional/com/proyectoensayo/MainActivity.java
+++ ProyectoEnsayo/app/src/main/java/ragp/provicional/com/proyectoensayo/MainActivity.java
@@ -2,6 +2,7 @@ package ragp.provicional.com.proyectoensayo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mostrarDatos(){
+        Toast.makeText(this, "HOLIWI", Toast.LENGTH_SHORT).show();
     }
 }
