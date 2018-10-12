26e8709c32224fe50507a6d15dace36774764de6
AlejandroLopez <alopez733@misena.edu.co>
2018-10-12 17:43:06 -0500

    Update ProyectoEnsayo/app/src/main/java/ragp/provicional/com/proyectoensayo/MainActivity.java

--- ProyectoEnsayo/app/src/main/java/ragp/provicional/com/proyectoensayo/MainActivity.java
+++ ProyectoEnsayo/app/src/main/java/ragp/provicional/com/proyectoensayo/MainActivity.java
@@ -11,39 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-        //hola mundo, peroon!!!!!!!
-=======
         Toast.makeText(this, "saquen las cerveza Prroooooo", Toast.LENGTH_SHORT).show();
-        //hola mundo!!!!!!!
->>>>>>> 92ae8950dd540906ca1cce7f917ed906d8fcdc99
-=======
-=======
+
         Toast.makeText(this, "saquen las cerveza Prroooooo", Toast.LENGTH_SHORT).show();
-        //hola mundo!!!!!!!
->>>>>>> c326db1efcebf20b41ff5a498a397f994c6b22dc
->>>>>>> a911a9962cbe1d01fced12b0b2643da15419cae3
 
     }
-    public void Toast2(){
+
+    public void Toast2() {
         Toast.makeText(this, "toast 2", Toast.LENGTH_SHORT).show();
-    
-    public void mostrarDatos(){
-        Toast.makeText(this, "HOLIWI", Toast.LENGTH_SHORT).show();
-    }
-    public void mostrarToast(){
-        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
 
+        public void mostrarDatos () {
+            Toast.makeText(this, "HOLIWI", Toast.LENGTH_SHORT).show();
+        }
+        public void mostrarToast () {
+            Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
+        }
+        public void mostrarToast2 () {
+            Toast.makeText(this, "Hola weyes", Toast.LENGTH_SHORT).show();
+        }
     }
-<<<<<<< HEAD
-    public void mostrarToast2(){
-        Toast.makeText(this, "Hola weyes", Toast.LENGTH_SHORT).show();
-
-    }
-
-=======
-    
->>>>>>> 92ae8950dd540906ca1cce7f917ed906d8fcdc99
 }
