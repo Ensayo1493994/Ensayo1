39a3891cba6d721d651ccb97d71c93b8902d06c8
RodrigoAGuspian <rodseiya20008@gmail.com>
2018-10-12 16:27:18 -0500

    agrego de toast

--- ProyectoEnsayo/app/src/main/java/ragp/provicional/com/proyectoensayo/MainActivity.java
+++ ProyectoEnsayo/app/src/main/java/ragp/provicional/com/proyectoensayo/MainActivity.java
@@ -2,6 +2,7 @@ package ragp.provicional.com.proyectoensayo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void mostrarToast(){
+        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
+    }
+
 }
